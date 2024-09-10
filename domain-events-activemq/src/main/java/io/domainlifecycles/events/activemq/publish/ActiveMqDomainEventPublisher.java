32af0657473e6b389c2c24b1994d71a3383600ec
Mario Herb <mario.herb@esentri.com>
2024-09-10 11:05:31 +0200

    ActiveMQ first draft

---
+++ domain-events-activemq/src/main/java/io/domainlifecycles/events/activemq/publish/ActiveMqDomainEventPublisher.java
@@ -0,0 +1,12 @@
+package io.domainlifecycles.events.activemq.publish;
+
+import io.domainlifecycles.domain.types.DomainEvent;
+import io.domainlifecycles.events.publish.DomainEventPublisher;
+
+public class ActiveMqDomainEventPublisher implements DomainEventPublisher {
+
+    @Override
+    public void publish(DomainEvent domainEvent) {
+
+    }
+}
