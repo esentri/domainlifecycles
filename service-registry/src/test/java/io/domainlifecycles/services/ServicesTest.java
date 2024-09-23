c61242681aad58eeac92b378cff18448f671b51c
Leon Völlinger <leon.voellinger@esentri.com>
2024-09-23 18:25:11 +0200

    - Finish Tests

--- service-registry/src/test/java/io/domainlifecycles/services/ServicesTest.java
+++
@@ -1,8 +0,0 @@
-package io.domainlifecycles.services;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ServicesTest {
-
-
-}
--- service-registry/src/test/java/io/domainlifecycles/services/api/ServiceTest.java
+++ service-registry/src/test/java/io/domainlifecycles/services/api/ServiceTest.java
@@ -1,6 +1,10 @@
 package io.domainlifecycles.services.api;
 
 import io.domainlifecycles.domain.types.ApplicationService;
+import io.domainlifecycles.domain.types.DomainService;
+import io.domainlifecycles.domain.types.OutboundService;
+import io.domainlifecycles.domain.types.QueryClient;
+import io.domainlifecycles.domain.types.Repository;
 import io.domainlifecycles.mirror.api.Domain;
 import io.domainlifecycles.mirror.reflect.ReflectiveDomainMirrorFactory;
 import io.domainlifecycles.services.Services;
@@ -13,13 +17,13 @@ import sample.TestQueryClient;
 import sample.TestRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ServiceProviderTest {
+class ServiceTest {
 
     private static ServiceProvider provider;
 
     @BeforeAll
     static void beforeAll() {
-        Domain.initialize(new ReflectiveDomainMirrorFactory("tests", "io.domainlifecycles"));
+        Domain.initialize(new ReflectiveDomainMirrorFactory("sample", "io.domainlifecycles"));
         provider = initServices();
     }
 
@@ -43,5 +47,41 @@ class ServiceProviderTest {
 
         assertThat(service).isNotNull();
         assertThat(service).isInstanceOf(TestApplicationService.class);
+    }
+
+    @Test
+    void testGetDomainServiceInstanceOk() {
+
+        DomainService service = provider.getDomainServiceInstance("sample.TestDomainService");
+
+        assertThat(service).isNotNull();
+        assertThat(service).isInstanceOf(TestDomainService.class);
+    }
+
+    @Test
+    void testGetRepositoryInstanceOk() {
+
+        Repository<?, ?> repository = provider.getRepositoryInstance("sample.TestRepository");
+
+        assertThat(repository).isNotNull();
+        assertThat(repository).isInstanceOf(TestRepository.class);
+    }
+
+    @Test
+    void testGetQueryClientOk() {
+
+        QueryClient<?> client = provider.getQueryClientInstance("sample.TestQueryClient");
+
+        assertThat(client).isNotNull();
+        assertThat(client).isInstanceOf(TestQueryClient.class);
+    }
+
+    @Test
+    void testGetOutboundServiceOk() {
+
+        OutboundService service = provider.getOutboundServiceInstance("sample.TestOutboundService");
+
+        assertThat(service).isNotNull();
+        assertThat(service).isInstanceOf(TestOutboundService.class);
     }
 }
