123862f775fc0ec9f94d530b84e5b8202237b0da
Leon Völlinger <leon.voellinger@esentri.com>
2024-11-07 16:26:02 +0100

    - Change License File according to Yumi Plugin Checks (add missing Line Break at beginning)

--- access/src/test/java/io/domainlifecycles/access/DlcAccessTest.java
+++ access/src/test/java/io/domainlifecycles/access/DlcAccessTest.java
@@ -1,3 +1,29 @@
+/*
+ *     ___
+ *     │   ╲                 _
+ *     │    ╲ ___ _ __  __ _(_)_ _
+ *     |     ╲ _ ╲ '  ╲╱ _` │ │ ' ╲
+ *     |_____╱___╱_│_│_╲__,_│_│_||_|
+ *     │ │  (_)╱ _│___ __ _  _ __│ |___ ___
+ *     │ │__│ │  _╱ -_) _│ ││ ╱ _│ ╱ -_|_-<
+ *     │____│_│_│ ╲___╲__│╲_, ╲__│_╲___╱__╱
+ *                      |__╱
+ *
+ *  Copyright 2019-2024 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package io.domainlifecycles.access;
 
 import io.domainlifecycles.access.classes.DefaultClassProvider;
@@ -43,7 +69,8 @@ class DlcAccessTest {
     @Test
     void testNewEnumInstanceOk() {
 
-        MyEnum anEnum = DlcAccess.newEnumInstance("ONE", "tests.shared.persistence.domain.oneToManyIdentityEnum.MyEnum");
+        MyEnum anEnum = DlcAccess.newEnumInstance("ONE",
+            "tests.shared.persistence.domain.oneToManyIdentityEnum.MyEnum");
 
         assertThat(anEnum).isNotNull();
         assertThat(anEnum.name()).isEqualTo("ONE");
@@ -52,7 +79,8 @@ class DlcAccessTest {
     @Test
     void testNewIdentityOk() {
 
-        Identity<Long> anIdentity = DlcAccess.newIdentityInstance(1L, "tests.shared.persistence.domain.inheritance.VehicleId");
+        Identity<Long> anIdentity = DlcAccess.newIdentityInstance(1L,
+            "tests.shared.persistence.domain.inheritance.VehicleId");
 
         assertThat(anIdentity).isNotNull();
         assertThat(anIdentity.value()).isEqualTo(1L);
